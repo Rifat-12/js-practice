f4d62b0b7b2c203cd5dc317bfbc897e455f16142
Rifat Hossain <rifatkajol010@gmail.com>
2022-12-26 23:45:02 +0600

    remove unnecessary files

--- p2 practice project/player.js
+++
@@ -1,9 +0,0 @@
-var clicks=0;
-function player() {
-    clicks += 1;
-    document.getElementById("play").innerHTML = clicks;
-  };
-  function player2() {
-    clicks += 1;
-    document.getElementById("play2").innerHTML = clicks;
-  };
