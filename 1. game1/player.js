03a8d240518d6e442a009e4e8f2ce1a7eb5ef05e
Rifat Hossain <rifatkajol010@gmail.com>
2022-12-26 23:52:33 +0600

    first game project

---
+++ 1. game1/player.js
@@ -0,0 +1,9 @@
+var clicks=0;
+function player() {
+    clicks += 1;
+    document.getElementById("play").innerHTML = clicks;
+  };
+  function player2() {
+    clicks += 1;
+    document.getElementById("play2").innerHTML = clicks;
+  };
